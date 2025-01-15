77edb228dd641034248404c3cdf6a3bd63d9c452
Sierra <sonicaradia@gmail.com>
2025-01-15 09:18:31 -0500

    Did experiment in index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,69 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+
+let healthPoints = 10;
+
+if (healthPoints >= 10) {
+    console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+    console.log("Hero is slightly damaged.");
+} else {
+    console.log("Hero is close to death.");
+}
+
+let currentDay = "Tuesday";
+
+if (currentDay = "Tuesday") {
+    console.log("Tacos for dinner!");
+}
+
+let weather = "Rainy";
+
+if (weather= "Rainy") {
+    console.log("Remember your umbrella!")
+}
+
+let items = 0;
+let fox = true;
+let stage = "Goomba";
+
+if (items == 0 && fox == true && stage == "Final Destination") {
+    console.log("For Glory!");
+} else {
+    console.log("For Fun!");
+}
+
+let date = "January 26th";
+
+if (date == "September 15th") {
+    console.log("It's Sierra's birthday!")
+} else {
+    console.log("It's just another day.")
+}
+
+
+let temperature = 65;
+
+if (temperature >= 75) {
+    console.log("Consider turning on the A/C.");
+} else if (temperature < 75 && temperature >= 65) {
+    console.log("Might be a good day for a picnic.");
+} else {
+    console.log("Bring a sweater!");
+}
+
+let phoneBattery = 3;
+
+if (phoneBattery == 100) {
+    console.log("Time to unplug the charger!");
+} else if (phoneBattery <= 99 && phoneBattery >= 40) {
+    console.log("All good!")
+} else if (phoneBattery <= 39 && phoneBattery >= 10) {
+    console.log("Running a bit low!")
+} else {
+    console.log("Mortal peril!");
+} 
+*/
